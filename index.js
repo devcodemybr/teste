d60588d733ddc8c2869060e6f3ebb7d21daed14d
nginpay <tozzi@nginpay.com>
2022-11-03 16:05:17 -0300

    sequelize

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const { User } = require('./models/index')
+
 
 const app = express()
 
 app.get('/', async (req, res) => {
     res.send('hello')
+})
+
+app.get('/users', async (req, res) => {
+    const list = await User.findAll()
+    return res.status(200).json(list)
 })
 
 app.listen(process.env.PORT, () => {
